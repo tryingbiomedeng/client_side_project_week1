d41af442e4df5d2b4ace3c2711e964421f8180e6
Ollie <olliebyrne2@outlook.com>
2023-07-31 10:29:57 +0100

    added css subfiles, bundle.js and set up package.json files

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-// Attach elements here
+const { getCountry } = require('./app.js')
 
+// Event listener on buttons for actions here
 
-const getCountry = () => {
-    fetch("https://yoururl.com") // Replace with server json URL
-        .then(resp => resp.json())
-        .then(pullCountry)
-        .catch(error => console.log(error));
-}
-
-const pullCountry = (data) => {
-    const randomCountry = data[Math.floor(Math.random()*data.length)];
-
-    const countryName = randomCountry.country; // Replace with tag used for country name in json
-    const capitalCity = randomCountry.capital_city; // Replace with tag used for capital city name in json
-
-    const allCities = data.map(country => country.capital_city); // Replace with tag used for capital city name in json
-    const randomCities = [];
-
-    while(randomCities.length < 3) {
-        const randomCity = allCities[Math.floor(Math.random()*allCities.length)];
-        if(randomCity !== capitalCity && !randomCities.includes(randomCity)) {
-            randomCities.push(randomCity);
-        }
-    }
-
-    console.log(countryName, capitalCity, ...randomCities);
-}
- // Need to change final product so that the 'countryName' is allocared to the question
- // And the capital city and three rnaodm capital cities are added to four identical button squares
+// Remember to convert to bundle when finished
